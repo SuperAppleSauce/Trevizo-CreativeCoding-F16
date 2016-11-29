d4df13210e1f5a96604e9d1d6e74dbd36c4aa26d
Alexis Trevizo <At2848@nyu.edu>
2016-11-29 02:25:53 -0500

    final update

--- CreativeCoding/Creative_Coding/CC_HW/week-12/API_Nytimes/sketch.js
+++ CreativeCoding/Creative_Coding/CC_HW/week-12/API_Nytimes/sketch.js
@@ -4,6 +4,8 @@
 var baseURL, apiKey, url, searchTerm, globalData;
 var iterator;
 var myTerms = [];
+var xPos = -200;
+var xVel = 0.05;
 
 function setup(){
 	noStroke();
@@ -11,34 +13,34 @@ function setup(){
 	createCanvas(600,600);
 	myTerms[0] = "tacos";
 	myTerms[1] = "cake";
-	myTerms[2] = "fire%20extinguisher";
+	myTerms[2] = "mushroom";
+	myTerms[3] = "GaGa";
 	baseURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
 	apiKey = "01b919a225a84165bed2acb3610cbe0b";
-	searchTerm = "tacos";
+
 	url = baseURL + "?api-key=" + apiKey + "&q=" + myTerms[0];
 	
 	loadJSON(url, dataReceived);
+
 
 }
 
 function draw(){
 	background(100,63,175,142);
+	fill(255);
+	textSize(18);
+	text("Relatively Hungry Turtle News", width/4, 50);
 	
-		if(globalData){ //test if data is available
+	if(globalData){ //test if data is available
 		
-
-		fill(255);
-		text(globalData.response.docs[iterator].pub_date, width/2, height/2);
-		
-
-
+		xPos+=xVel;
+		text(globalData.response.docs[0].headline.main, xPos, height/2);	
 	}
 	else { //still loading!
 		println("there's no data yet!");
-
-		
-		
 	}	
+
+	
 
 }
 function loadNewData(){
@@ -50,13 +52,20 @@ function dataReceived(incomingData){
 }
 
 function mouseClicked(){
+	if(globalData){
 	iterator++;
+
+}
+	else{
+		println("please wait...loading next article");
+	}
+
 	if(iterator > myTerms.length){
 		iterator = 0;	
 	}
 	println(iterator);
-	console.log(globalData.response.docs[iterator].pub_date);
-	// url = baseURL + "?api-key=" + apiKey + "&q=" + myTerms[iterator];
-	// loadNewData();
+	console.log(globalData.response.docs[iterator].snippet);
+	url = baseURL + "?api-key=" + apiKey + "&q=" + myTerms[iterator-2];
+	loadNewData();
 }
 
